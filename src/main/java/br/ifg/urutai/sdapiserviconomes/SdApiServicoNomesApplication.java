6116b64025d8dfc13fb77ff71f206bb51dba4d72
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-06-07 10:58:07 -0300

    init

---
+++ src/main/java/br/ifg/urutai/sdapiserviconomes/SdApiServicoNomesApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.urutai.sdapiserviconomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdApiServicoNomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdApiServicoNomesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifg/urutai/sdapiserviconomes/SdApiServicoNomesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifg.urutai.sdapiserviconomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdApiServicoNomesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
