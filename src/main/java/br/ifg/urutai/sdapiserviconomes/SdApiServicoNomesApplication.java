aa221c357c616b83a0e06b5f688c1f4368249d18
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-06-07 11:05:51 -0300

    Serviço de nomes

--- src/main/java/br/ifg/urutai/sdapiserviconomes/SdApiServicoNomesApplication.java
+++ src/main/java/br/ifg/urutai/sdapiserviconomes/SdApiServicoNomesApplication.java
@@ -2,8 +2,11 @@ package br.ifg.urutai.sdapiserviconomes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SdApiServicoNomesApplication {
 
     public static void main(String[] args) {
